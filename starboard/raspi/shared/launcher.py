71416d5ea046f5046d26868ad6d489f5d9528739
Kaido Kert <kaidokert@google.com>
2023-04-23 12:25:54 -0700

    Fix up retry logic in Raspi launcher
    Re-applies fixes from reverted #106 after clean-up and refactor
    of retry code.
    
    b/278447902

--- starboard/raspi/shared/launcher.py
+++ starboard/raspi/shared/launcher.py
@@ -65,14 +65,23 @@ class Launcher(abstract_launcher.AbstractLauncher):
   # pexpect times out each second to allow Kill to quickly stop a test run
   _PEXPECT_TIMEOUT = 1
 
+  # SSH shell command retrres
+  _PEXPECT_SPAWN_RETRIES = 5
+
+  # pexpect.sendline retries
+  _PEXPECT_SENDLINE_RETRIES = 3
+
+  # Old process kill reties
+  _KILL_RETRIES = 3
+
   _PEXPECT_SHUTDOWN_SLEEP_TIME = 3
   # Time to wait after processes were killed
   _PROCESS_KILL_SLEEP_TIME = 10
 
   # Retrys for getting a clean prompt
   _PROMPT_WAIT_MAX_RETRIES = 5
-  # Wait up to 30 seconds for the password prompt from the raspi
-  _PEXPECT_PASSWORD_TIMEOUT_MAX_RETRIES = 30
+  # Wait up to 10 seconds for the password prompt from the raspi
+  _PEXPECT_PASSWORD_TIMEOUT_MAX_RETRIES = 10
   # Wait up to 900 seconds for new output from the raspi
   _PEXPECT_READLINE_TIMEOUT_MAX_RETRIES = 900
   # Delay between subsequent SSH commands
@@ -80,6 +89,10 @@ class Launcher(abstract_launcher.AbstractLauncher):
 
   # This is used to strip ansi color codes from pexpect output.
   _PEXPECT_SANITIZE_LINE_RE = re.compile(r'\x1b[^m]*m')
+
+  # Exceptions to retry
+  _RETRY_EXCEPTIONS = (pexpect.TIMEOUT, pexpect.ExceptionPexpect,
+                       pexpect.exceptions.EOF, OSError)
 
   def __init__(self, platform, target_name, config, device_id, **kwargs):
     # pylint: disable=super-with-arguments
@@ -114,6 +127,9 @@ class Launcher(abstract_launcher.AbstractLauncher):
 
   def _InitPexpectCommands(self):
     """Initializes all of the pexpect commands needed for running the test."""
+
+    # Ensure no trailing slashes
+    self.out_directory = self.out_directory.rstrip('/')
 
     # TODO(b/218889313): This should reference the bin/ subdir when that's
     # used.
@@ -161,6 +177,14 @@ class Launcher(abstract_launcher.AbstractLauncher):
     self.test_command = (f'{test_base_command} {test_success_output}'
                          f'{test_failure_output}')
 
+  # pylint: disable=no-method-argument
+  def _CommandBackoff():
+    time.sleep(Launcher._INTER_COMMAND_DELAY_SECONDS)
+
+  @retry.retry(
+      exceptions=_RETRY_EXCEPTIONS,
+      retries=_PEXPECT_SPAWN_RETRIES,
+      backoff=_CommandBackoff)
   def _PexpectSpawnAndConnect(self, command):
     """Spawns a process with pexpect and connect to the raspi.
 
@@ -182,7 +206,7 @@ class Launcher(abstract_launcher.AbstractLauncher):
 
     # pylint: disable=unnecessary-lambda
     @retry.retry(
-        exceptions=(pexpect.TIMEOUT,),
+        exceptions=Launcher._RETRY_EXCEPTIONS,
         retries=Launcher._PEXPECT_PASSWORD_TIMEOUT_MAX_RETRIES,
         backoff=lambda: self.shutdown_initiated.is_set(),
         wrap_exceptions=False)
@@ -201,8 +225,13 @@ class Launcher(abstract_launcher.AbstractLauncher):
 
     _inner()
 
+  @retry.retry(
+      exceptions=_RETRY_EXCEPTIONS,
+      retries=_PEXPECT_SENDLINE_RETRIES,
+      wrap_exceptions=False)
   def _PexpectSendLine(self, cmd):
     """Send lines to Pexpect and record the last command for logging purposes"""
+    logging.info('sending >> : %s ', cmd)
     self.last_run_pexpect_cmd = cmd
     self.pexpect_process.sendline(cmd)
 
@@ -210,7 +239,7 @@ class Launcher(abstract_launcher.AbstractLauncher):
     """Reads all lines from the pexpect process."""
     # pylint: disable=unnecessary-lambda
     @retry.retry(
-        exceptions=(pexpect.TIMEOUT,),
+        exceptions=Launcher._RETRY_EXCEPTIONS,
         retries=Launcher._PEXPECT_READLINE_TIMEOUT_MAX_RETRIES,
         backoff=lambda: self.shutdown_initiated.is_set(),
         wrap_exceptions=False)
@@ -247,10 +276,9 @@ class Launcher(abstract_launcher.AbstractLauncher):
         time.sleep(self._PEXPECT_SHUTDOWN_SLEEP_TIME)
         try:
           self.pexpect_process.readlines()
-        except pexpect.TIMEOUT:
-          logging.info('Timeout exception during cleanup command: %s',
-                       self.last_run_pexpect_cmd)
-          pass
+        except Launcher._RETRY_EXCEPTIONS:
+          logging.exception('Exception during cleanup command: %s',
+                            self.last_run_pexpect_cmd)
         logging.info('Done sending dmesg')
 
       # Send ctrl-c to the raspi and close the process.
@@ -268,11 +296,15 @@ class Launcher(abstract_launcher.AbstractLauncher):
 
     retry.with_retry(
         lambda: self.pexpect_process.expect(self._RASPI_PROMPT),
-        exceptions=(pexpect.TIMEOUT,),
+        exceptions=Launcher._RETRY_EXCEPTIONS,
         retries=Launcher._PROMPT_WAIT_MAX_RETRIES,
         backoff=backoff,
         wrap_exceptions=False)
 
+  @retry.retry(
+      exceptions=_RETRY_EXCEPTIONS,
+      retries=_KILL_RETRIES,
+      backoff=_CommandBackoff)
   def _KillExistingCobaltProcesses(self):
     """If there are leftover Cobalt processes, kill them.
 
@@ -332,8 +364,15 @@ class Launcher(abstract_launcher.AbstractLauncher):
         for cmd in first_run_commands:
           if not self.shutdown_initiated.is_set():
             self._PexpectSendLine(cmd)
-            line = self.pexpect_process.readline()
-            self.output_file.write(line)
+
+            def _readline():
+              line = self.pexpect_process.readline()
+              self.output_file.write(line)
+
+            retry.with_retry(
+                _readline,
+                exceptions=Launcher._RETRY_EXCEPTIONS,
+                retries=Launcher._PROMPT_WAIT_MAX_RETRIES)
         self._WaitForPrompt()
         self.output_file.flush()
         self._Sleep(self._INTER_COMMAND_DELAY_SECONDS)
@@ -344,6 +383,9 @@ class Launcher(abstract_launcher.AbstractLauncher):
         self._PexpectSendLine(self.test_command)
         self._PexpectReadLines()
 
+    except retry.RetriesExceeded:
+      logging.exception('Command retry exceeded (cmd: %s)',
+                        self.last_run_pexpect_cmd)
     except pexpect.EOF:
       logging.exception('pexpect encountered EOF while reading line. (cmd: %s)',
                         self.last_run_pexpect_cmd)
