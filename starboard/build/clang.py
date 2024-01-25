b7d2c4a7c75e35dbe8cb6cca2858f6c52b65708a
Kaido Kert <kaidokert@google.com>
2024-01-25 15:37:39 -0800

    Bump clang to 17

--- starboard/build/clang.py
+++ starboard/build/clang.py
@@ -18,4 +18,4 @@ from starboard.tools.build import ClangSpecification
 
 def GetClangSpecification():
   """Gets the ClangSpecification instance for this project."""
-  return ClangSpecification('16-init-18429-g4c443eb8-1', '16.0.0')
+  return ClangSpecification('-17-init-17284-gb2557213-1', '17.0.0')
